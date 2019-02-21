643b943db0c24d56c06c2c1d44257982288dd817
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-02-21 14:08:30 +0100

    Adapted categories

--- src/administration/QuestionForm.js
+++ src/administration/QuestionForm.js
@@ -25,12 +25,10 @@ const questionTypes = [
 ]
 
 const competences = [
-  { id: 1, name: 'Category 1'},
-  { id: 2, name: 'Category 2'},
-  { id: 3, name: 'Category 3'},
-  { id: 4, name: 'Category 4'},
-  { id: 5, name: 'Category 5'},
-  { id: 6, name: 'Category 6', inhibitUserAnalysis: true}
+  { id: 1, name: 'Wertversprechen'},
+  { id: 2, name: 'Qualität und Service'},
+  { id: 3, name: 'Haufigkeit der Nutzung'},
+  { id: 4, name: 'Zufriedenheit'}
 ]
 
 let sequence = 0
